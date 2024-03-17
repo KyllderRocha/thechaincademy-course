2c9e11a2f50400d6dcffba882af0e71d4c82f28c
KyllderRocha <kyllder.rocha@gmail.com>
2024-03-17 19:45:23 -0300

    Deploy contract and test

--- contracts/Migrations.sol
+++ contracts/Migrations.sol
@@ -1,64 +1,24 @@
 // SPDX-License-Identifier: MIT
-pragma solidity ^0.8.0;
+pragma solidity >=0.4.22 <0.9.0;
 
 contract Migrations {
-    // Variáveis de estado
+    address public owner;
+    uint public last_completed_migration;
 
-    //Número inteiro.
-    uint256 public myUint;
-    //Endereço Ethereum.
-    address public myAddress;
-    //String
-    string public myString;
-    //Booleano
-    bool public myBool;
-    //Valor de 32 bytes
-    bytes32 public myBytes32;
-
-    // Variável de estado privada
-
-    // Proprietário do contrato
-    address private owner;
-
-    // Erros
-    error UnauthorizedAccess(address caller, address owner);
-
-    // Modificador
-    modifier onlyOwner {
-         if (msg.sender != owner) {
-            revert UnauthorizedAccess(msg.sender, owner);
-        }
-        _;
-    }
-    
-    // Modificador
-    modifier onlyPositiveValues (uint256 value) {
-        require(value > 0, "Only Positive Values in this function");
-        _;
+    modifier restricted() {
+        if (msg.sender == owner) _;
     }
 
-    // Construtor
     constructor() {
         owner = msg.sender;
-        myUint = 123;
-        myAddress = msg.sender;
-        myString = "Hello, world!";
-        myBool = true;
-        myBytes32 = keccak256(abi.encodePacked("solidity"));
     }
 
-    // Função pública para atualizar myUint
-    function setMyUint(uint256 newValue) public onlyOwner onlyPositiveValues(newValue) {
-        myUint = newValue;
+    function setCompleted(uint completed) public restricted {
+        last_completed_migration = completed;
     }
 
-    // Função pública para atualizar myString
-    function setMyString(string memory newValue) public onlyOwner {
-        myString = newValue;
-    }
-
-    // Função pública para retornar o valor de myUint
-    function getMyUint() public view returns (uint256) {
-        return myUint;
+    function upgrade(address new_address) public restricted {
+        Migrations upgraded = Migrations(new_address);
+        upgraded.setCompleted(last_completed_migration);
     }
 }
